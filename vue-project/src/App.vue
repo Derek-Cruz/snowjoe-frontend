acbc364ccc09fd43621dd746eb3197573ba033d5
Derek-Cruz <Derekcruz94@gmail.com>
2022-05-04 10:04:51 -0700

    working on css.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,19 +1,20 @@
 
 <template>
   <Header :header="header" />
+  <h1>Quiz 1 - HTML/CSS/JS Practice</h1>
   <form>
-    <ol>
-      <li v-for="question in questions" :key="question.question">
-        {{ question.question }}
-        <div v-for="c of question.choices" :key="c">
+    <ul>
+      <li v-for="(question, index) in questions" :key="question.question" class="testing-li">
+        <p class="testing-p">
+          {{ index }}. {{ question.question }}
+        </p>
+        <div v-for="c of question.choices" :key="c" class="testing-div">
         <input type="radio" name="choice" v-model="answer" :value="c" />
         {{ c }}
       </div>
       </li>
-    </ol>
-    <button type="button" @click="submit">submit</button>
+    </ul>
   </form>
-  <p>score: {{ score }}</p>
 </template>
 
 <script>
@@ -45,31 +46,42 @@ export default {
       header: 'Quiz Application',
       questions,
       score: 0,
-      questionIndex: 0,
-      question: questions[0],
       answer: "",
     };
-  },
-  methods: {
-    submit() {
-      const { answer, question, questions, questionIndex } = this;
-      if (answer === question.rightAnswer) {
-        this.score++;
-      }
-      if (questionIndex < questions.length) {
-        this.questionIndex++;
-        this.question = { ...questions[this.questionIndex] };
-      }
-    },
   },
 };
 </script>
 
 <style>
-
 body {
   background: #f4f6f6;
   margin: 0;
 }
 
+ ul {
+   padding: 0 20px;
+ }
+
+.testing-li {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  width: 100%;
+  border-radius: 5px;
+  background: white;
+  margin-bottom: 30px;
+  list-style-type: none;
+  padding: 20px 0;
+}
+
+.testing-p {
+  padding: 0px 25px;
+      margin-top: 0;
+  margin-bottom: 13px;
+  font-size: 1.2rem;
+}
+
+.testing-div {
+  padding: 0px 21px;
+  margin-bottom: 10px;
+  font-size: 1rem;
+}
 </style>
